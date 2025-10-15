d2f453f5cc59dd4d8fdce1d28f3c2e9074999cb3
Simon Sawert <simon@sawert.se>
2025-10-15 05:46:40 +0200

    feat: Add support to disable stringer (#40)
    * feat: Add support to disable methods
    
    Currently `godump` always uses the stringer interface if defined. This
    is not always desirable if you want to check the internals of a type.
    
    This adds a feature with the same name used for [`spew`][spew]
    (`DisableMethods`) which can be set to true to disable this feature.
    
    [spew]: https://github.com/davecgh/go-spew/blob/8991bc29aa16c548c550c7ff78260e27b9ab7c73/spew/config.go#L52-L54
    
    * fix: Use existing stringer type
    
    * Rename to `WithDisableStringer`
    
    * Improve test, only call `asStringer` once

--- godump.go
+++ godump.go
@@ -29,11 +29,12 @@ const (
 
 // Default configuration values for the Dumper.
 const (
-	defaultMaxDepth      = 15
-	defaultMaxItems      = 100
-	defaultMaxStringLen  = 100000
-	defaultMaxStackDepth = 10
-	initialCallerSkip    = 2
+	defaultDisableStringer = false
+	defaultMaxDepth        = 15
+	defaultMaxItems        = 100
+	defaultMaxStringLen    = 100000
+	defaultMaxStackDepth   = 10
+	initialCallerSkip      = 2
 )
 
 // defaultDumper is the default Dumper instance used by Dump and DumpStr functions.
@@ -90,6 +91,7 @@ type Dumper struct {
 	maxStringLen       int
 	writer             io.Writer
 	skippedStackFrames int
+	disableStringer    bool
 
 	// callerFn is used to get the caller information.
 	// It defaults to [runtime.Caller], it is here to be overridden for testing purposes.
@@ -156,16 +158,26 @@ func WithSkipStackFrames(n int) Option {
 	}
 }
 
+// WithDisableStringer will determine if the stringer value for types that
+// implement the stringer interface should be render instead of the actual type.
+func WithDisableStringer(b bool) Option {
+	return func(d *Dumper) *Dumper {
+		d.disableStringer = b
+		return d
+	}
+}
+
 // NewDumper creates a new Dumper with the given options applied.
 // Defaults are used for any setting not overridden.
 func NewDumper(opts ...Option) *Dumper {
 	d := &Dumper{
-		maxDepth:     defaultMaxDepth,
-		maxItems:     defaultMaxItems,
-		maxStringLen: defaultMaxStringLen,
-		writer:       os.Stdout,
-		colorizer:    nil, // ensure no detection is made if we don't need it
-		callerFn:     runtime.Caller,
+		maxDepth:        defaultMaxDepth,
+		maxItems:        defaultMaxItems,
+		maxStringLen:    defaultMaxStringLen,
+		disableStringer: defaultDisableStringer,
+		writer:          os.Stdout,
+		colorizer:       nil, // ensure no detection is made if we don't need it
+		callerFn:        runtime.Caller,
 	}
 	for _, opt := range opts {
 		d = opt(d)
@@ -472,11 +484,7 @@ func (d *Dumper) printValue(w io.Writer, v reflect.Value, indent int, visited ma
 			}
 			indentPrint(w, indent+1, d.colorize(colorYellow, symbol)+field.Name)
 			fmt.Fprint(w, "	=> ")
-			if s := d.asStringer(fieldVal); s != "" {
-				fmt.Fprint(w, s)
-			} else {
-				d.printValue(w, fieldVal, indent+1, visited)
-			}
+			d.printValue(w, fieldVal, indent+1, visited)
 			fmt.Fprintln(w)
 		}
 		indentPrint(w, indent, "")
@@ -553,6 +561,10 @@ func (d *Dumper) printValue(w io.Writer, v reflect.Value, indent int, visited ma
 
 // asStringer checks if the value implements fmt.Stringer and returns its string representation.
 func (d *Dumper) asStringer(v reflect.Value) string {
+	if d.disableStringer {
+		return ""
+	}
+
 	val := v
 	if !val.CanInterface() {
 		val = forceExported(val)
